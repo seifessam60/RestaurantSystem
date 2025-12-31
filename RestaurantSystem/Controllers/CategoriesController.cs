e452b5869792aedda8a90e1a670f70cb9bfae33c
Seif Essam <seifessam60@gmail.com>
2025-12-31 14:21:11 +0200

    Add Categories Controller with Index and Details views

---
+++ RestaurantSystem/Controllers/CategoriesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantSystem.Data;
+using System.Threading.Tasks;
+
+namespace RestaurantSystem.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories
+                .Include(c => c.MenuItems)
+                .ToListAsync();
+            return View(categories);
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var category = await _context.Categories
+                .Include(c => c.MenuItems)
+                .FirstOrDefaultAsync(c => c.Id == id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+    }
+}
